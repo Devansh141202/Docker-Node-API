a6645ff5f358dd2d5eefca9b0a92cedc1be86026
devansh141202 <20ce061@charusat.edu.in>
2023-01-05 23:52:19 +0530

    updated

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -13,20 +13,6 @@ exports.createProduct = async (req, res) => {
   });
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 exports.getProductDetails = async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
@@ -35,29 +21,5 @@ exports.getProductDetails = async (req, res) => {
   res.status(200).json({
     success: true,
     product,
-  });
-};
-exports.updateProduct = async (req, res) => {
-  let product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!product) {
-    return res.status(400).send({ message: "Product not found" });
-  }
-  res.status(200).json({
-    success: true,
-    product,
-  });
-};
-
-exports.deleteProduct = async (req, res) => {
-  const product = await Product.findByIdAndDelete(req.params.id);
-  if (!product) {
-    return res.status(400).send({ message: "Product not found" });
-  }
-  await product.remove();
-  res.status(200).json({
-    success: true,
-    message: "Product Deleted",
   });
 };
